68e86c93f8a07653da233525c12da1879ce54df0
19시_웹개발_ksy <19__ksy@DESKTOP-F0Q81EV>
2021-03-11 21:39:31 +0900

    commit

--- 07_java_class_array/src/step7_01/classArray/ClassArrayEx01.java
+++ 07_java_class_array/src/step7_01/classArray/ClassArrayEx01.java
@@ -1,10 +1,72 @@
 package step7_01.classArray;
 
+// 클ㄹ래스 배열 생성 예시 [1]
+
+class Product {
+	
+	String name;
+	int price;
+	
+}
+
 public class ClassArrayEx01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Product[] productList = new Product[3];
+		
+		productList[0] = new Product();
+		productList[1] = new Product();
+		productList[2] = new Product();
+		
+		productList[0].name = "기계식 키보드";
+		productList[0].price= 45000;
+		
+		productList[1].name = "무소음 마우스";
+		productList[1].price = 35000;
+		
+		productList[2].name = "장패드";
+		productList[2].price = 10000;
+		
+
+		for (int i = 0; i < productList.length; i++) {
+			System.out.println("\nname : " + productList[i].name);
+			System.out.println("price : " + productList[i].price);
+			System.out.println();
+		}
+		
+		
+		//[응용] 생각해보기
+		
+//		Product temp = productList[1];
+		
+		Product[] temp1 = productList;
+		
+		Product temp2 = productList[1];
+		
+		String temp3 = productList[0].name;
+		
+		int temp4 = productList[0].price;
+		
+		Product temp = productList[2];
+		System.out.println("temp : " + temp);
+		System.out.println("productList[2] : " + productList[2]);
+		
+		System.out.println("temp.name : " + temp.name);
+		System.out.println("productList[2].name : " + productList[2].name);
+		
+		temp.name = "장패드1";
+		System.out.println("temp.name : " + temp.name);
+		System.out.println("productList[2].name : " + productList[2].name);
+		
+		productList[2].name = "장패드2";
+		System.out.println("temp.name : " + temp.name);
+		System.out.println("productList[2].name : " + productList[2].name);
+		
 	}
 
 }
+
+
+
+
